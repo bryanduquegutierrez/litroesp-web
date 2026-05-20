138d172780f8dc799aaeb913ba671bc80aeabfbb
bryanduquegutierrez <bryanduquegutierrez@gmail.com>
2026-05-20 13:49:35 +0200

    se añaden mas paises a la pagina web

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,18 @@
 import { Banner } from "./components/Banner";
 import { Contact } from "./components/Contact";
 import { CTA } from "./components/CTA";
-import { EuropeHero } from "./components/EuropeHero";
 import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Nav } from "./components/Nav";
 import { Stats } from "./components/Stats";
+import { WorldHero } from "./components/WorldHero";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <Nav />
       <Banner />
-      <EuropeHero />
+      <WorldHero />
       <Features />
       <Stats />
       <Contact />
